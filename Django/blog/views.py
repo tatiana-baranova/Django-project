3c5be8fa03fc387d7aa4e4128c903524102e8b39
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-07-23 23:00:52 +0300

    update html

--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse("Hello, welcome to my blog")
+    news = [
+        {
+            'title': 'News 1',
+            'text': 'This is the first news item.',
+            'date': '2026-01-23',
+        },
+        {
+            'title': 'News 2',
+            'text': 'This is the second news item.',
+            'date': '2026-01-24',
+            'author': 'Author 2',
+        },
+    ]
+    
+    data = {
+        'news': news,
+        'title': 'Home Page',
+    }
+    return render(request, 'blog/home.html', data)
 
 def contact(request):
-    return HttpResponse("This is the contact page of my blog.")
+    return render(request, 'blog/contact.html', {'title': 'Contact Page'})
